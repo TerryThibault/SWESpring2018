ebc0748308b8d786ec888480f38587a6118c1464
Jad <zeineddine.jad@gmail.com>
2018-02-27 03:51:25 -0500

    added login route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,30 @@
 var express = require('express');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
 var router = express.Router();
 
 /* GET home page. */
+
+
+app.post('/login',
+passport.authenticate('local', { failureRedirect: '/login' }),
+ function(req, res) {
+   res.redirect('/');
+ });
+
+passport.use(new Strategy(
+  function(username, password, cb) {
+    //db doesnt currently exist
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
