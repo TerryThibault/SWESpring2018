d7c1bd8b506856aaf7a435885077f4e549a3b0ab
Jad <zeineddine.jad@gmail.com>
2018-03-20 11:01:18 -0400

    added new route to login

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,32 +1,46 @@
 var express = require('express');
-var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
 var router = express.Router();
 
 /* GET home page. */
 
+//
+// app.post('/login',
+// passport.authenticate('local', { failureRedirect: '/login' }),
+//  function(req, res) {
+//    res.redirect('/');
+//  });
+//
+// passport.use(new Strategy(
+//   function(username, password, cb) {
+//     //db doesnt currently exist
+//     db.users.findByUsername(username, function(err, user) {
+//       if (err) { return cb(err); }
+//       if (!user) { return cb(null, false); }
+//       if (user.password != password) { return cb(null, false); }
+//       return cb(null, user);
+//     });
+//   }));
+//
 
-app.post('/login',
-passport.authenticate('local', { failureRedirect: '/login' }),
- function(req, res) {
-   res.redirect('/');
- });
 
-passport.use(new Strategy(
-  function(username, password, cb) {
-    //db doesnt currently exist
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
+router.get('/account', function(req, res, next) {
+  var obj = {username : 'jadz',
+            email : 'jzeineddine@ufl.edu' };
+  res.render('index', { account_info: obj });
 
-
-
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
 });
+
+
+
+router.get('/login', function(req, res, next)){
+  function authenticationMiddleware () {
+  return function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/')
+  }
+}
+}
 
 module.exports = router;
