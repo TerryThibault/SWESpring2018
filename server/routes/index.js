441be4278affbd700202830bdb911d7ca0d892cf
Jad <zeineddine.jad@gmail.com>
2018-03-26 21:45:29 -0400

    refactoring login/signup to connect with any middleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,46 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-
-//
-// app.post('/login',
-// passport.authenticate('local', { failureRedirect: '/login' }),
-//  function(req, res) {
-//    res.redirect('/');
-//  });
-//
-// passport.use(new Strategy(
-//   function(username, password, cb) {
-//     //db doesnt currently exist
-//     db.users.findByUsername(username, function(err, user) {
-//       if (err) { return cb(err); }
-//       if (!user) { return cb(null, false); }
-//       if (user.password != password) { return cb(null, false); }
-//       return cb(null, user);
-//     });
-//   }));
-//
-
-
-router.get('/account', function(req, res, next) {
-  var obj = {username : 'jadz',
-            email : 'jzeineddine@ufl.edu' };
-  res.render('index', { account_info: obj });
-
-});
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var db = require('./db');
 
 
 
-router.get('/login', function(req, res, next)){
-  function authenticationMiddleware () {
-  return function (req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-    res.redirect('/')
-  }
-}
-}
+
+
+
 
 module.exports = router;
