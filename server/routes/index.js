0eb1ce3e06f7b2d8b24902dc70a2031d2c976b06
Jad <zeineddine.jad@gmail.com>
2018-03-26 22:09:03 -0400

    needs to connect with datastore, need to add frontend

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
-var db = require('./db');
+var db = require('../db');
 
 //Setup passport Strategy
 passport.use(new Strategy(
@@ -30,10 +30,15 @@ passport.use(new Strategy(
 router.use(passport.initialize());
 router.use(passport.session());
 
+router.get('/login',
+function(req,res){
+  res.render('login');
+})
+
 router.post('/login',
   passport.authenticate('local', { failureRedirect: '/' }),//change this later
   function(req, res) {
-    res.redirect('/');
+    res.send("logged in");
   });
 
 module.exports = router;
