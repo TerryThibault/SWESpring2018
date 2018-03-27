f19a50f30ddf5706484b2d986a638ba3c373d76e
Jad <zeineddine.jad@gmail.com>
2018-03-26 21:57:55 -0400

    added userstore, needs connection to db

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,36 @@ var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
 var db = require('./db');
 
+//Setup passport Strategy
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+  passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
 
+router.use(passport.initialize());
+router.use(passport.session());
 
-
-
+router.post('/login',
+  passport.authenticate('local', { failureRedirect: '/' }),//change this later
+  function(req, res) {
+    res.redirect('/');
+  });
 
 module.exports = router;
